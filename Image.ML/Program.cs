35a9442e3807aec8ad522dff90802c39d46d54eb
Infarh <shmachilin@yandex.ru>
2021-03-14 11:30:58 +0300

    Добавьте файлы проекта.

---
+++ Image.ML/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Image.ML
+{
+    // https://code-ai.mk/how-to-use-ml-net-model-builder-for-image-classification/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
